0936d35000e775f6275df932e8c2a7cc7de4dd2d
Brian Roth <brianroth@gmail.com>
2013-05-01 22:11:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuoteBoard::Application.config.secret_key_base = 'e518f86390602ad05ce05b96ce07e9d90508591110c7c5c21eaecc2018e580a9c3cee21e115a00fd93455544fcc82bf58bd24f968e89bdbd41c739cd096d5166'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuoteBoard::Application.config.session_store :encrypted_cookie_store, key: '_quote_board_session'
